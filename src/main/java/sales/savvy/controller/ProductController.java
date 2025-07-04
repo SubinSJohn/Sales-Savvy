de6aa980074b13032b2ee8f1a1015a129bb86ac7
SubinSJohn <subinsjohn28@gmail.com>
2025-07-04 18:18:55 +0530

    updating deletion

--- src/main/java/sales/savvy/controller/ProductController.java
+++ src/main/java/sales/savvy/controller/ProductController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import sales.savvy.entity.Product;
@@ -44,9 +46,10 @@ public class ProductController {
 		return "success";
 	}
 	
-	@PostMapping("/deleteProduct")
-	public void deleteProduct(Long id) {
+	@DeleteMapping("/deleteProduct")
+	public String deleteProduct(@RequestParam Long id) {
 		service.deleteProduct(id);
+		return "success";
 	}
 	
 }
